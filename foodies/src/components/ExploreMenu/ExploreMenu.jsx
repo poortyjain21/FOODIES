cde2b281073cf68b17bbbd9544a1fc7480f728eb
PoortyJain <poortyjain@gmail.com>
2025-06-04 23:56:31 +0530

    scroll functionality added

--- foodies/src/components/ExploreMenu/ExploreMenu.jsx
+++ foodies/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,19 +1,39 @@
-import React from "react";
+import React, { useRef } from "react";
 import { categories } from "../../assets/assets";
-import "./ExploreMenu.css"
+import "./ExploreMenu.css";
 
 const ExploreMenu = () => {
+  const scrollRef = useRef(null);
+
+  const scroll = (direction) => {
+    const { current } = scrollRef;
+    if (direction === "left") {
+      current.scrollLeft -= 200; // scroll left by 200px
+    } else {
+      current.scrollLeft += 200; // scroll right by 200px
+    }
+  };
+
   return (
     <div className="explore-menu position-relative mt-2">
       <h1 className="d-flex align-items-center justify-content-between">
         Explore Our Menu
         <div className="d-flex gap-4">
-          <i className="bi bi-arrow-left-circle-fill scroll-icon"></i>
-          <i className="bi bi-arrow-right-circle-fill scroll-icon"></i>
+          <i
+            className="bi bi-arrow-left-circle-fill scroll-icon"
+            onClick={() => scroll("left")}
+          ></i>
+          <i
+            className="bi bi-arrow-right-circle-fill scroll-icon"
+            onClick={() => scroll("right")}
+          ></i>
         </div>
       </h1>
       <p>Explore curated lists of dishes from top categories</p>
-      <div className="d-flex justify-content-between gap-4 overflow-auto explore-menu-list ">
+      <div
+        className="d-flex justify-content-between gap-4 overflow-auto explore-menu-list "
+        ref={scrollRef}
+      >
         {categories.map((item, index) => {
           return (
             <div key={index} className="text-center explore-menu-list-item">
@@ -28,6 +48,7 @@ const ExploreMenu = () => {
           );
         })}
       </div>
+      <hr/>
     </div>
   );
 };
